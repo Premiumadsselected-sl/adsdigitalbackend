cad8adef4881b6c33ac793bb8c8df3f9317f4f06
Premiumadsselected <administracion@adsselected.com>
2024-06-19 00:51:33 +0200

    fix -- Prisma migrations on vercel deploy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body,Post } from '@nestjs/common'
+import { Controller, Body, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthDto } from 'src/dto/auth.dto'
 import { ApiTags } from '@nestjs/swagger'
