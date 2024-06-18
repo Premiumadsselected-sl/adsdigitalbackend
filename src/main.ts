fdcfbf23095c4b4089939874a34e8c1cfe2ebc70
Premiumadsselected <administracion@adsselected.com>
2024-06-19 00:20:09 +0200

    fix -- Prisma migrations on vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api/swagger', app, document)
 
-  // app.useGlobalGuards(new AuthGuard(
-  //   new JwtService({ secret: process.env.API_JWT_SECRET })
-  // ))
+  app.useGlobalGuards(new AuthGuard(
+    new JwtService({ secret: process.env.API_JWT_SECRET })
+  ))
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -31,15 +31,12 @@ async function bootstrap() {
     }
   }))
 
-  // app.enableCors({
-  //   origin: process.env.API_CORS_ORIGIN,
-  //   methods: 'GET, POST, PUT, PATCH, DELETE',
-  //   allowedHeaders: 'Content-Type, Authorization',
-  //   credentials: true
-  // })
-
-  app.enableCors()
-
+  app.enableCors({
+    origin: process.env.API_CORS_ORIGIN,
+    methods: 'GET, POST, PUT, PATCH, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
+  })
 
   await app.listen(process.env.PORT || 3001)
 
